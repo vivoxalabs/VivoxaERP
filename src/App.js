a4cccdc76729ffe4478e6f3e481334dc0213e63f
Hirantha <hiranthapeiris404@gmail.com>
2019-09-23 13:33:54 +0530

    dashboard-init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 //import logo from './logo.svg';
 import NavBar from "./components/NavBar";
 import Login from "./components/Login";
+import Dashboard from "./components/dashboard";
 import "./App.css";
 import { BrowserRouter as Router} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
@@ -16,6 +17,9 @@ class App extends Component {
         }} />
         <Route path="/console" exact strict render={()=>{
           return (<NavBar/>)
+        }} />
+        <Route path="/dash" exact strict render={()=>{
+          return (<Dashboard/>)
         }} />
       </Router>
     );
