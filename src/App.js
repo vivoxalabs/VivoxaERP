6573a41790ad708cc46d544dce9094e909784e8f
Hirantha <hiranthapeiris404@gmail.com>
2019-12-23 00:10:13 +0530

    new router adjestments

--- src/App.js
+++ src/App.js
@@ -2,23 +2,45 @@ import React, { Component } from "react";
 //import logo from './logo.svg';
 import NavBar from "./components/NavBar";
 import Login from "./components/Login";
-import Dashboard from "./components/dashboard";
+import PreEntry from "./components/PreEntry";
+
+//import Dashboard from "./components/dashboard";
 import "./App.css";
-import { BrowserRouter as Router} from 'react-router-dom';
-import Route from 'react-router-dom/Route';
+import { BrowserRouter as Router } from "react-router-dom";
+import Route from "react-router-dom/Route";
 
 class App extends Component {
   state = {};
   render() {
     return (
       <Router>
-        <Route path="/" exact strict render={()=>{
-          return (<Login/>)
-        }} />
-        <Route path="/console" exact strict render={()=>{
-          return (<NavBar/>)
-        }} />
-        
+        <Route
+          path="/"
+          exact
+          strict
+          render={() => {
+            return <Login />;
+          }}
+        />
+        <Route
+          path="/console"
+          exact
+          strict
+          render={() => {
+            return <NavBar />;
+          }}
+        />
+        <Route
+          path="/pre-entry"
+          render={() => {
+            return (
+              <React.Fragment>
+                <NavBar />
+                <PreEntry />
+              </React.Fragment>
+            );
+          }}
+        />
       </Router>
     );
   }
--- src/components/PreEntry.jsx
+++ src/components/PreEntry.jsx
@@ -8,7 +8,8 @@ import {
   Select,
   MenuItem,
   CssBaseline,
-  Paper
+  Paper,
+  FormControl
 } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
@@ -16,12 +17,15 @@ const useStyles = makeStyles(theme => ({
     width: "auto",
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
-    marginTop : theme.spacing(10)
+    marginTop: theme.spacing(10)
   },
   paper: {
     marginTop: theme.spacing(3),
     marginBottom: theme.spacing(3),
     padding: theme.spacing(3)
+  },
+  form:{
+      minWidth:50
   }
 }));
 
@@ -47,74 +51,79 @@ export default function PreEntry() {
           Pre-Enter Customer Details
         </Typography>
         <Paper className={classes.paper}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={6}>
-            <TextField
-              required
-              id="firstName"
-              name="firstName"
-              label="First Name"
-              fullWidth
-              autoComplete="fname"
-            />
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={6}>
+              <TextField
+                required
+                id="firstName"
+                name="firstName"
+                label="First Name"
+                fullWidth
+                autoComplete="fname"
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+              <TextField
+                required
+                id="lastName"
+                name="lastName"
+                label="Last Name"
+                fullWidth
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12} md={12} lg={12}>
+              <TextField
+                required
+                id="address1"
+                name="address1"
+                label="Address Line 1"
+                fullWidth
+                autoComplete="address1"
+              />
+            </Grid>
+            <Grid item xs={12} md={12} lg={12}>
+              <TextField
+                required
+                id="address2"
+                name="address2"
+                label="Address Line 2"
+                fullWidth
+                autoComplete="address2"
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+              <TextField
+                required
+                id="city"
+                name="city"
+                label="City*"
+                fullWidth
+                autoComplete="city"
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+              <FormControl className={classes.form}>
+                <InputLabel id="demo-simple-select-label">
+                  Vehicle Type
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={values.type}
+                  inputProps={{
+                    name: "type",
+                    id: "type-simple"
+                  }}
+                  onChange={handleChange}
+                >
+                  <MenuItem value={"Auto"}>Auto</MenuItem>
+                  <MenuItem value={"Manual"}>Manual</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={6} lg={6}>
-            <TextField
-              required
-              id="lastName"
-              name="lastName"
-              label="Last Name"
-              fullWidth
-              autoComplete="lname"
-            />
-          </Grid>
-          <Grid item xs={12} md={12} lg={12}>
-            <TextField
-              required
-              id="address1"
-              name="address1"
-              label="Address Line 1"
-              fullWidth
-              autoComplete="address1"
-            />
-          </Grid>
-          <Grid item xs={12} md={12} lg={12}>
-            <TextField
-              required
-              id="address2"
-              name="address2"
-              label="Address Line 2"
-              fullWidth
-              autoComplete="address2"
-            />
-          </Grid>
-          <Grid item xs={12} md={6} lg={6}>
-            <TextField
-              required
-              id="city"
-              name="city"
-              label="City*"
-              fullWidth
-              autoComplete="city"
-            />
-          </Grid>
-          <Grid item xs={12} md={6} lg={6}>
-            <InputLabel htmlFor="type">Vehicle Type</InputLabel>
-            <Select
-              value={values.type}
-              inputProps={{
-                name: "type",
-                id: "type-simple"
-              }}
-              onChange={handleChange}
-            >
-              <MenuItem value={"Auto"}>Auto</MenuItem>
-              <MenuItem value={"Manual"}>Manual</MenuItem>
-            </Select>
-          </Grid>
-        </Grid>
         </Paper>
-        
       </main>
     </React.Fragment>
   );
